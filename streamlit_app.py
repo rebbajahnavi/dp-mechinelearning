96b65d7a593a1179ab869aeac3d7c7e015ddf991
rebbajahnavi <jahnavireb16@gmail.com>
2026-09-16 22:38:14 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,10 @@
 import streamlit as st
-
+import pandas as pd
 st.title('Mechine Learning App')
-
 st.info('This app builds a mechining learning model!')
+with st.expander('Data'):
+  st.write('**Raw Data**')
+  df=pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv?utm_source=chatgpt.com')
+
+  
+
