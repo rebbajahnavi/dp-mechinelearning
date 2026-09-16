e093c28cc632a8b8abcc4b4178d5444b2c9cf057
rebbajahnavi <jahnavireb16@gmail.com>
2026-09-16 22:49:50 +0530

    Fix indentation and add missing variable assignment

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,5 +5,5 @@ st.info('This app builds a mechining learning model!')
 with st.expander('Data'):
   st.write('**Raw Data**')
   df = pd.read_csv("https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv")
-  
+  df
 
